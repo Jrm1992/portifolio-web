09ddd8660009f13353fc4bafd24afd0e8970739e
Jrm1992 <marquesjoseroberto@proton.me>
2024-05-22 20:30:09 -0300

    initial commit

---
+++ src/main/java/com/example/portifolioweb/PortifolioWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.portifolioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifolioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifolioWebApplication.class, args);
+	}
+
+}
