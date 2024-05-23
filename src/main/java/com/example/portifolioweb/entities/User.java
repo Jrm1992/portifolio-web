88cbc66769225e9a27b3f0f83deb0be3cf72feb5
Jrm1992 <marquesjoseroberto@proton.me>
2024-05-23 20:51:36 -0300

    added attribute serialVersionUID to User entity

--- src/main/java/com/example/portifolioweb/entities/User.java
+++ src/main/java/com/example/portifolioweb/entities/User.java
@@ -1,8 +1,12 @@
 package com.example.portifolioweb.entities;
 
+import java.io.Serializable;
 import java.util.Objects;
 
-public class User {
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private int id;
     private String name;
     private String email;
