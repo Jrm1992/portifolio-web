11a8da2521f16f9424e24ab93513fe27fbacd35b
Jrm1992 <marquesjoseroberto@proton.me>
2024-05-25 16:48:49 -0300

    updated User entity

--- src/main/java/com/example/portifolioweb/entities/User.java
+++ src/main/java/com/example/portifolioweb/entities/User.java
@@ -1,9 +1,13 @@
 package com.example.portifolioweb.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -11,15 +15,17 @@ public class User implements Serializable {
     private String name;
     private String email;
     private String phone;
+    private String password;
 
     public User() {
     }
 
-    public User(int id, String name, String email, String phone) {
+    public User(int id, String name, String email, String phone, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
+        this.password = password;
     }
 
     public int getId() {
@@ -46,13 +52,19 @@ public class User implements Serializable {
     public void setPhone(String phone) {
         this.phone = phone;
     }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return getId() == user.getId() && Objects.equals(getName(), user.getName()) && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getPhone(), user.getPhone());
+        return getId() == user.getId() && Objects.equals(getName(), user.getName()) && Objects.equals(getEmail(), user.getEmail()) && Objects.equals(getPhone(), user.getPhone()) && Objects.equals(getPassword(), user.getPassword());
     }
 
     @Override
