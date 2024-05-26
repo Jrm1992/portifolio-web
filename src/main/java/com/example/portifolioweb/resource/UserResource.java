1cddd65d397a44b7a6f26708db9150dcb5cbd617
Jrm1992 <marquesjoseroberto@proton.me>
2024-05-26 15:22:06 -0300

    created UserResource

--- src/main/java/com/example/portifolioweb/resource/UserResource.java
+++ src/main/java/com/example/portifolioweb/resource/UserResource.java
@@ -1,9 +1,44 @@
 package com.example.portifolioweb.resource;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.portifolioweb.entities.User;
+import com.example.portifolioweb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping()
+    public List<User> findAll() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
+    @PostMapping()
+    public void insert(@RequestBody User user) {
+        userService.insert(user);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+
+    @PutMapping("/{id}")
+    public User update(@PathVariable Long id, @RequestBody User user) {
+        return userService.update(id, user);
+    }
+
 }
